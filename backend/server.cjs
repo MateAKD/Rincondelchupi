f691a5adf1c4ccef94f8f4b941e4fc317b24edac
MateAKD <mateopedace@gmail.com>
2025-06-25 20:57:08 -0300

    Update server.cjs

--- backend/server.cjs
+++ backend/server.cjs
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001; // <--- Cambiado acá
 
 app.use(cors());
 app.use(express.json());
@@ -29,4 +29,4 @@ app.post('/productos', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Servidor backend escuchando en http://localhost:${PORT}`);
-}); 
+});
